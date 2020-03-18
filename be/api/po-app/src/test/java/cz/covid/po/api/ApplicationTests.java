491e2148d71f7e90f9ca3bfc2fa55da00652f105
Vladimír Řehoř <vladmir.rehor@creativedock.cz>
2020-03-18 08:03:57 +0100

    Fixed run errors

--- be/api/po-app/src/test/java/cz/covid/po/api/ApplicationTests.java
+++ be/api/po-app/src/test/java/cz/covid/po/api/ApplicationTests.java
@@ -4,9 +4,11 @@ import cz.covid.po.api.app.Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = Application.class)
 @Profile("test")
+@ActiveProfiles("test")
 class ApplicationTests {
 
     @Test
