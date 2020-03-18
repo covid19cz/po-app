1df91303673b99d3b34bfeed763198a3c21c261f
Betlista <Betlista+github@gmail.com>
2020-03-18 20:03:39 +0100

    HealtCheckResult -> HealthCheckResult

--- be/api/po-domain/src/main/java/cz/covid/po/api/domain/repository/HealthCheckResultRepository.java
+++ be/api/po-domain/src/main/java/cz/covid/po/api/domain/repository/HealthCheckResultRepository.java
@@ -1,13 +1,12 @@
 package cz.covid.po.api.domain.repository;
 
-import cz.covid.po.api.domain.model.Address;
-import cz.covid.po.api.domain.model.HealtCheckResult;
+import cz.covid.po.api.domain.model.HealthCheckResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface HealthCheckResultRepository extends JpaRepository<HealtCheckResult, Long> {
+public interface HealthCheckResultRepository extends JpaRepository<HealthCheckResult, Long> {
 
-    List<HealtCheckResult> findAllByResultSentAtIsNull();
+    List<HealthCheckResult> findAllByResultSentAtIsNull();
 
 }
