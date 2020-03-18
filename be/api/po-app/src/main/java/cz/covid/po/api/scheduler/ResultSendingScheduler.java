5a99d9b913c5ba79fcf8b6e64b2ef6b1e673dfa8
Betlista <Betlista+github@gmail.com>
2020-03-18 16:58:26 +0100

    sending logic implemented

--- be/api/po-app/src/main/java/cz/covid/po/api/scheduler/ResultSendingScheduler.java
+++ be/api/po-app/src/main/java/cz/covid/po/api/scheduler/ResultSendingScheduler.java
@@ -2,11 +2,13 @@ package cz.covid.po.api.scheduler;
 
 import cz.covid.po.api.domain.model.HealtCheckResult;
 import cz.covid.po.api.domain.repository.HealthCheckResultRepository;
+import cz.covid.po.api.integration.profisms.service.ProfiSmsSender;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Log
@@ -16,13 +18,19 @@ public class ResultSendingScheduler {
     @Autowired
     HealthCheckResultRepository healthCheckResultRepository;
 
+    @Autowired
+    ProfiSmsSender profiSmsSender;
+
     @Scheduled(cron = "${api.scheduler.cron}")
     public void sendResults() {
         log.info("result sending scheduler running...");
         List<HealtCheckResult> allNotSentYet = healthCheckResultRepository.findAllByResultSentAtIsNull();
-        // TODO: send SMS
-        // TODO: update all
-        System.out.println();
+        for (HealtCheckResult healtCheckResult:allNotSentYet) {
+            String phoneNumber = healtCheckResult.getHealthCheck().getPerson().getPhoneNumber();
+            profiSmsSender.sendSms(phoneNumber, "Vysledek vaseho testu je: " + healtCheckResult.getResultPositive());
+            healtCheckResult.setResultSentAt(OffsetDateTime.now());
+            healthCheckResultRepository.save(healtCheckResult);
+        }
     }
 
 }
