a1e84fe8629760f6e08f784699ec68cbb848f1cb
Jonas Vaclavek <jonas.vaclavek@gmail.com>
2020-03-19 13:01:21 +0100

    update host name resolver path construction

--- fe/app/src/service/HostnameResolver.ts
+++ fe/app/src/service/HostnameResolver.ts
@@ -1,26 +1,15 @@
 const API_BASE_PATH = "/api/v1";
 const API_PORT = "8080";
-const API_PREFIX = "api.";
-const APP_PREFIX = "app.";
 const PROXY_URL = "http://localhost:1337";
-const DEFAULT_PROTOCOL = "http://";
 
 const getApiUrl = () => {
-  // Return URL from config if set in .env
-  if (process.env.REACT_APP_API_URL) {
-    return process.env.REACT_APP_API_URL;
-  }
-
-  // Return localhost with port
-  if (getHostName() === "localhost") {
-    return PROXY_URL.concat("/")
+  return (
+    process.env.REACT_APP_API_URL ||
+    PROXY_URL.concat("/")
       .concat(getHostName())
       .concat(":")
-      .concat(API_PORT);
-  }
-
-  // Resolve hostname
-  return getApiPrefix().concat(getHostName());
+      .concat(API_PORT)
+  );
 };
 
 const getApiUrlWithBasePath = () => {
@@ -28,46 +17,11 @@ const getApiUrlWithBasePath = () => {
 };
 
 const getHostName = () => {
-  return typeof window !== "undefined"
-    ? window.location.hostname.replace(new RegExp(APP_PREFIX), "")
-    : "localhost";
-};
-
-const getProtocol = () => {
-  return typeof window !== "undefined"
-    ? window.location.protocol.concat("//")
-    : DEFAULT_PROTOCOL;
-};
-
-const getApiPrefix = () => {
-  return getProtocol().concat(API_PREFIX);
-};
-
-const getLandingPageUrl = () => {
-  return getProtocol().concat(getHostName());
-};
-
-const getLandingPageUrlWithParams = (params: object) =>
-  appendParamsToUrl(getLandingPageUrl(), params);
-
-const appendParamsToUrl = (url: string, params: any) => {
-  let str = "";
-  for (const key of Object.keys(params)) {
-    if (str !== "") {
-      str += "&";
-    }
-    str += key + "=" + params[key];
-  }
-
-  return `${url}?${str}`;
+  return typeof window !== "undefined" ? window.location.hostname : "localhost";
 };
 
 export const HostnameResolver = {
   getApiUrl,
   getApiUrlWithBasePath,
-  getApiPrefix,
-  getHostName,
-  getProtocol,
-  getLandingPageUrl,
-  getLandingPageUrlWithParams
+  getHostName
 };
