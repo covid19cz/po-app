614542ede82276431ac3c58abfe36254030de089
Honza <jan.vodera@seznam.cz>
2020-03-19 20:17:43 +0100

    add length and null check for phone normalizer

--- be/api/po-app/src/main/java/cz/covid/po/api/helper/PhoneNormalizerHelper.java
+++ be/api/po-app/src/main/java/cz/covid/po/api/helper/PhoneNormalizerHelper.java
@@ -7,12 +7,16 @@ public class PhoneNormalizerHelper {
     private static final String DOUBLE_ZERO = "00";
 
     public String normalizePhone(final String originalPhone) {
-        String normalizedPhone = originalPhone.trim();
+        if (originalPhone != null && originalPhone.length() > 1) {
+            String normalizedPhone = originalPhone.trim();
 
-        if (DOUBLE_ZERO.equals(normalizedPhone.substring(0, 2))) {
-            normalizedPhone = normalizedPhone.replaceFirst(DOUBLE_ZERO, "+");
+            if (DOUBLE_ZERO.equals(normalizedPhone.substring(0, 2))) {
+                normalizedPhone = normalizedPhone.replaceFirst(DOUBLE_ZERO, "+");
+            }
+
+            return normalizedPhone;
         }
 
-        return normalizedPhone;
+        throw new IllegalArgumentException("Phone is missing or too short");
     }
 }
